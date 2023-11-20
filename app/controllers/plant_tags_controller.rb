4248c9a43a8398f7117cafbd6910380b445d2d4b
Joao Mateus <jaomateus@gmail.com>
2023-11-20 21:25:03 +0000

    finish project

--- app/controllers/plant_tags_controller.rb
+++ app/controllers/plant_tags_controller.rb
@@ -6,15 +6,11 @@ class PlantTagsController < ApplicationController
 
   def create
     @plant = Plant.find(params[:plant_id])
-    @tag = Tag.find(params[:plant_tag][:tag])
-    @plant_tag = PlantTag.new(plant: @plant, tag: @tag)
-    @plant_tag.save
+    @tags = Tag.where(id: params[:plant_tag][:tag])
+    @tags.each do |tag|
+      plant_tag = PlantTag.new(plant: @plant, tag: tag)
+      plant_tag.save
+    end
     redirect_to garden_path(@plant.garden)
   end
-
-  private
-
-  # def plant_tag_params
-  #   params.require(:plant_tag).permit(:)
-  # end
 end
--- app/models/plant_tag.rb
+++ app/models/plant_tag.rb
@@ -1,4 +1,5 @@
 class PlantTag < ApplicationRecord
   belongs_to :plant
   belongs_to :tag
+  validates :tag, uniqueness: {scope: :plant, message: "already added"}
 end
